e70292e4658fe56742de8c6282ceb85a5cc5da19
Paula González toscano <gonzaleztoscano.pau@gmail.com>
2023-05-29 14:53:02 -0300

    Version_alfa_creacion1erTemplate

--- premiosPlatziapp/polls/views.py
+++ premiosPlatziapp/polls/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
+from .models import Question
+
+
 
 def index(request):
-    return HttpResponse ("Hello World")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html",{
+        "latest_question_list":latest_question_list
+        })
+
+def detail(request,question_id):
+    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+ 
+
+def results(request,question_id):
+    return HttpResponse(f"Estas viendo los resultados de pregunta numero  {question_id}")
+
+
+def vote(request,question_id):
+    return HttpResponse(f"Estas votando a la pregunta numero {question_id}")
