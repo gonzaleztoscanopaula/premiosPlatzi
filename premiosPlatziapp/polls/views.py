18573b38880a90530d35349fa1e534491397cfc6
Paula González toscano <gonzaleztoscano.pau@gmail.com>
2023-05-29 21:03:52 -0300

    Creacion de template detailhtml

--- premiosPlatziapp/polls/views.py
+++ premiosPlatziapp/polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 
 
 
@@ -12,14 +12,28 @@ def index(request):
         })
 
 def detail(request,question_id):
-    question= question = get_object_or_404(Question, pk=question_id)
+    question= get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html",{
         "question": question
     })
 
 def results(request,question_id):
-    return HttpResponse(f"Estas viendo los resultados de pregunta numero  {question_id}")
+    question= get_object_or_404( Question, pk=question_id)
+    return render(request, "polls/results.html", {
+        "question": question
+    })
 
 
 def vote(request,question_id):
-    return HttpResponse(f"Estas votando a la pregunta numero {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice= question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "No elegiste una respuesta"
+        })
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results",args=(question.id,)))
